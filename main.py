5df4b07ab57283696f338d8eaabe2da5499a6797
formvo <formvo@naver.com>
2021-02-08 00:43:02 +0900

    길드원 데이터 업데이트
    길드원 데이터 업데이트 알고리즘 수정

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ def updateExcelFile():
         html = urlopen("https://maple.gg/u/" + urllib.parse.quote(load_ws.cell(row=i, column=1).value))
         bsObject = BeautifulSoup(html, "html.parser")
 
-        load_ws.cell(i, 4, bsObject.find('h1', class_='user-summary-floor font-weight-bold').get_text().replace(" ","").replace("\n",""))
+        load_ws.cell(i, 2, bsObject.find('ul', class_='user-summary-list').find_all('li', class_='user-summary-item')[0].get_text().replace("Lv.",""))
+        load_ws.cell(i, 3, bsObject.find('ul', class_='user-summary-list').find_all('li', class_='user-summary-item')[1].get_text())
+        load_ws.cell(i, 4, bsObject.find('h1', class_='user-summary-floor font-weight-bold').get_text().replace(" ","").replace("\n","").replace("층",""))
         load_ws.cell(i, 5, bsObject.find('small', class_='user-summary-duration').get_text())
 
     load_wb.save('../MapleGuildSuro/party.xlsx')
